65ed80e31e0b0ecb08435b469edea4de0359005e
dumderedum <batters21@yandex.ru>
2020-04-15 11:31:13 +0300

    joke

---
+++ 15.04.2020/joke.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ping -c 1 ya.ru &> /dev/null; c=$?
+joke=`curl -s https://api.chucknorris.io/jokes/rando | jq -r ".value"`
+linecount=`cat jokes.txt | wc -l`
+
+if [[ $c == 0 ]] && [[ $joke != 'null' ]]; then
+    echo $joke
+    echo $joke >> jokes.txt
+elif [[ $linecount != 0 ]]; then
+    cat jokes.txt | shuf -n1
+else
+    echo " ( ͡° ͜ʖ ͡°)   F  ¯\_(ツ)_/¯ "
+fi
